2da58eb011fedc326a879abd87046469b45c9821
Nadz31 <nadiya_khaan@yahoo.com>
2018-09-30 16:10:06 +0200

    Array

---
+++ array.js
@@ -0,0 +1,9 @@
+let myFavFood =[];
+console.log('the value of my array is'); + (myFavFood =['Pizza','burger','pasta','cake']);
+console.log(myFavFood);
+
+let myFavAnimals= ['Cat','Dog','Dinosaur','Donkey'];
+console.log(myFavAnimals);
+myFavAnimals.push('babypig');
+console.log(myFavAnimals);
+ 
